e7b1b2cb79ebb5bccd8647f9d88da107e39981f5
RasyKy <kyrasy547@gmail.com>
2025-11-11 09:08:11 +0700

    product service

---
+++ product-service/src/main/java/com/coffeeshop/product_service/model/Product.java
@@ -0,0 +1,15 @@
+package com.coffeeshop.product_service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+
+@Document(collection = "products")
+@Data
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+}
---
+++ product-service/src/main/java/com/coffeeshop/product_service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.coffeeshop.product_service.repository;
+
+import com.coffeeshop.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    // Spring Data MongoDB will automatically create methods like
+    // findAll(), findById(), save(), etc. You don't need to write them.
+}
